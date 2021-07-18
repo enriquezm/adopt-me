010960c64af8d6442f922dbe602a6606c77534f4
Myles Enriquez <enri.myles@gmail.com>
2021-07-17 23:09:41 -0700

    Add SearchParams component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchParams from './SearchParams';
 
 const App = () => {
-  return <div>Hello, World!</div>;
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
